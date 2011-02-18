36c7d09d3487dbeb1a598bae2f63131cdd82bea2
Sean Suggs <ssuggs@digitalfunction.com>
2011-02-18 15:29:41 -0800

    added template to make it easier to submit work

--- exercises/home/demo.js
+++ exercises/home/demo.js
@@ -1,5 +1,99 @@
 $(document).ready(function() {
     /* jQuery Code Here */
+    
+/////////////
+//EXERCISES//
+/////////////
+    
+console.log("SELECTING//////////");
+//Selecting
+    
+    console.warn("1) Select all of the div elements that have a class of module. \n");
+    console.log(/*$("div.module")*/);
+    console.log("\n");
 
+    console.warn("2) Come up with three selectors that you could use to get the third item in the #myList unordered list. Which is the best to use? Why? \n");
+    console.log(/*yourcodehere*/);
+    console.log(/*yourcodehere*/);
+    console.log(/*yourcodehere*/);
+    console.log("Your reason here");
+    console.log("\n");
+    
+    
+    console.warn("3) Select the label for the search input using an attribute selector. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    console.warn("4) Figure out how many elements on the page are hidden (hint: .length). \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    console.warn("5) Figure out how many image elements on the page have an alt attribute. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    console.warn("6) Select all of the odd table rows in the table body. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+
+    
+console.log("\n \n TRAVERSING//////////");
+//Traversing     
+
+    console.warn("1) Select all of the image elements on the page; log each image's alt attribute. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("2) Select the search input text box, then traverse up to the form and add a class to the form. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("3) Select the list item inside #myList that has a class of 'current' and remove that class from it; add a class of 'current' to the next list item. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    
+    console.warn("4) Select the select element inside #specials; traverse your way to the submit button. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("5) Select the first list item in the #slideshow element; add the class 'current' to it, and then add a class of 'disabled' to its sibling elements. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+
+    
+    
+console.log("\n \n MANIPULATING//////////");
+//Manipulating     
+
+    console.warn("1) Add five new list items to the end of the unordered list #myList. Hint: for (var i = 0; i<5; i++) { ... } \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("2) Remove the odd list items \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("3) Add another h2 and another paragraph to the last div.module \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    
+    console.warn("4) Add another option to the select element; give the option the value 'Wednesday' \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
+    
+    console.warn("5) Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it. \n");
+    console.log(/*YOUR CODE HERE*/);
+    console.log("\n");
+    
 
 });
