c569dbab1eb116612c590392a44d6b36808992f9
suggsgested <ssuggs@digitalfunction.com>
2011-03-11 22:43:24 -0600

    added chapter 7 exercise "External Content"

---
+++ exercises/CHAPTER-7/ExternalContent/demo.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    /* jQuery Code Here */
+    
+/////////////
+//EXERCISES//
+/////////////
+        
+console.log("\n \n External Content//////////");
+//External Content
+
+
+});
+
+// NOTE:
+// URL TO /exercises/data/blog.html for this jsFiddle is
+// "/gh/get/response.html/suggsgested/jQuery-Fundamentals-Exercises/tree/master/exercises/CHAPTER-7/ExternalContent/"
